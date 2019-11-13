0c5817859e66dea8040f99937ed18200642aeb80
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-11-13 16:23:53 +0100

    chore: add documentation, bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## [Unreleased]
-### Changed
-- Add tokenizer for org-mode
+## [1.0.0] - 13.11.2019
+### Added
+- Export to [json resume](jsonresume.org).
 
-[Unreleased]: https://github.com/your-name/resume/compare/0.1.1...HEAD
+[1.0.0]: https://github.com/scoiatael/resume/releases/v1.0.0
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resume "0.1.0-SNAPSHOT"
+(defproject resume "1.0.0"
   :description "Resume-from-org generator"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,7 +8,6 @@
                  [org.clojure/core.match "0.3.0"]]
   :aliases {"build-resume-json" ["run" "-m" "resume.core/export-resume-json"]}
   :repl-options {:init-ns resume.core}
-  :profiles { ;:dev {:dependencies  [org.clojure/tools.trace "0.7.10"]}
-             :test {:dependencies [[midje "1.9.9"]
+  :profiles {:test {:dependencies [[midje "1.9.9"]
                                    [nubank/matcher-combinators "1.2.4"]]
                     :plugins [[lein-midje "3.2.1"]]}})
